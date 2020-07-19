71fb803ede1abf70b043f819c3376927a4ff1095
alikarani <aliasgharkarani125@hotmail.com>
2020-07-20 01:20:16 +0500

    2 dicee

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    print('Left Button Pressed');
+                  },
+                  child: Image.asset('images/dice1.png'))),
+          Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    print('Right Button Pressed');
+                  },
+                  child: Image.asset('images/dice1.png'))),
+        ],
+      ),
+    ));
+  }
+}
